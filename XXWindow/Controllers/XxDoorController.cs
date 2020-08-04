7622fb6c2993036188d303599bb020edc6892296
zkl <1090890128@qq.com>
2020-08-04 10:40:27 +0800

    添加项目文件。

---
+++ XXWindow/Controllers/XxDoorController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using XXWindow.ViewModel;
+
+namespace XXWindow.Controllers
+{
+    public class XxDoorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult ProductShow()
+        {
+            return View();
+        }
+
+        public JsonResult GetProductByPage(int Page)
+        {
+            var i = Page.ToString();
+            ProductVM products = new ProductVM();
+            products.Total = new Random().Next(20, 100);
+            var model = new List<ProductPageModel>()
+            {
+                new ProductPageModel(){ ImgSrc = i },
+                new ProductPageModel(){ ImgSrc = i },
+                new ProductPageModel(){ ImgSrc = i },
+                new ProductPageModel(){ ImgSrc = i },
+                 new ProductPageModel(){ ImgSrc = i },
+                new ProductPageModel(){ ImgSrc = i },
+                new ProductPageModel(){ ImgSrc = i },
+                new ProductPageModel(){ ImgSrc = i },
+            };
+
+            products.Products = model;
+            return new JsonResult(products);
+        }
+    }
+
+   
+
+}
---
+++ XXWindow/ViewModel/ProductVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XXWindow.ViewModel
+{
+    public class ProductVM
+    {
+        public int Total { get; set; }
+
+        public List<ProductPageModel> Products { get; set; }
+    }
+
+
+    public class ProductPageModel
+    {
+
+        public string ImgSrc { get; set; }
+    }
+}
